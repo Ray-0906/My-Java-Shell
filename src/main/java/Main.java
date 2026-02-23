8ea26371e99bdd8235934cbc538218560f1ea4f7
Ray-0906 <astra3716@gmail.com>
2026-02-23 10:40:39 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,39 +1,46 @@
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Scanner;
- 
+
 public class Main {
-    public static List<String> comList = List.of("echo", "exit", "type", "pwd");
-     
+
+    static Path currentDir;
+    static Path homeDir;
+
+    public static List<String> comList = List.of("echo", "exit", "type", "pwd", "cd");
+
     static void typeHandler(String typeString) {
         // String typeString=input.substring(5);
-        String  res = typeString + ": not found";
+        String res = typeString + ": not found";
         if (comList.contains(typeString)) {
-             res = typeString + " is a shell builtin";
+            res = typeString + " is a shell builtin";
         } else {
-           
-            for(String path: System.getenv("PATH").split(":")){
-                File file = new File(path + "/" +typeString);
-                if(file.exists() && file.canExecute()){
-                    res =  typeString+" is " +file.getAbsolutePath();
+
+            for (String path : System.getenv("PATH").split(":")) {
+                File file = new File(path + "/" + typeString);
+                if (file.exists() && file.canExecute()) {
+                    res = typeString + " is " + file.getAbsolutePath();
                     break;
                 }
             }
-           
+
         }
         System.out.println(res);
     }
+
     boolean isExecutable(String path) {
         File file = new File(path);
         return file.exists() && file.canExecute();
     }
-    
+
     static void exechandler(String input) {
-         String[] args= input.split(" ");
-         String comString = args[0];   
-        for(String path: System.getenv("PATH").split(":")){
-            File file = new File(path + "/" +comString);
-            if(file.exists() && file.canExecute()){
+        String[] args = input.split(" ");
+        String comString = args[0];
+        for (String path : System.getenv("PATH").split(":")) {
+            File file = new File(path + "/" + comString);
+            if (file.exists() && file.canExecute()) {
                 // args[0] = file.getAbsolutePath();
                 try {
                     ProcessBuilder pb = new ProcessBuilder(args);
@@ -47,15 +54,39 @@ public class Main {
                 return;
             }
         }
-          System.out.println(input + ": command not found");
+        System.out.println(input + ": command not found");
 
     }
-   
 
-    
+    static void cdHandler(String input) {
+        String path = input.substring(3);
+        if (path.equals("~")) {
+            currentDir = homeDir;
+        } else if (path.startsWith("/")) {
+            Path newPath = Paths.get(path);
+            if (newPath.toFile().exists() && newPath.toFile().isDirectory()) {
+                currentDir = newPath;
+            } else {
+                System.out.println("cd: " + path + ": No such file or directory");
+            }
+        }
+        else {
+            Path newPath = currentDir.resolve(path);
+            if (newPath.toFile().exists() && newPath.toFile().isDirectory()) {
+                currentDir = newPath;
+            } else {
+                System.out.println("cd: " + path + ": No such file or directory");
+            }
+        }
+
+    }
 
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
+
+        currentDir = Paths.get(System.getProperty("user.dir"));
+        ;
+        homeDir = Paths.get(System.getProperty("user.home")).toAbsolutePath();
         while (true) {
             System.out.print("$ ");
             Scanner scanner = new Scanner(System.in);
@@ -70,12 +101,15 @@ public class Main {
                 case "exit":
                     System.exit(0);
                 case "pwd":
-                    System.out.println(new File("").getAbsolutePath());
-                    break;    
+                    System.out.println(currentDir.toAbsolutePath().toString());
+                    break;
 
                 case "type":
                     String typeString = input.substring(5);
                     typeHandler(typeString);
+                    break;
+                case "cd":
+                    cdHandler(input);
                     break;
                 default:
                     // System.out.println(input + ": command not found");
