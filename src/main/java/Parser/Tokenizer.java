0f9ec27266b54c826a525f9b4302a19a9aae49ca
Ray-0906 <astra3716@gmail.com>
2026-02-25 12:46:08 +0530

    codecrafters submit [skip ci]

--- src/main/java/Parser/Tokenizer.java
+++ src/main/java/Parser/Tokenizer.java
@@ -3,45 +3,53 @@ package Parser;
 import java.util.List;
 
 public class Tokenizer {
-     public static List<String> tokenize(String input) {
+    public static List<String> tokenize(String input) {
         List<String> tokens = new java.util.ArrayList<>();
         StringBuilder current = new StringBuilder();
 
         boolean inSingleQuote = false;
         boolean inDoubleQuote = false;
-        boolean isbackslash = false;
 
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
 
-            // Handle single quote
-            if (isbackslash) {
-                current.append(c);
-                isbackslash = false;
-                continue;
-            }
-            if (c == '\\' && !inSingleQuote) {
-                isbackslash = true;
-                continue;
-            }
+            // SINGLE QUOTES
             if (c == '\'' && !inDoubleQuote) {
                 inSingleQuote = !inSingleQuote;
+                continue;
             }
-            // Handle double quote
-            else if (c == '"' && !inSingleQuote) {
+
+            // DOUBLE QUOTES
+            if (c == '"' && !inSingleQuote) {
                 inDoubleQuote = !inDoubleQuote;
+                continue;
             }
-            // Handle whitespace (only if outside quotes)
-            else if (Character.isWhitespace(c) && !inSingleQuote && !inDoubleQuote) {
+
+            // BACKSLASH inside DOUBLE QUOTES
+            if (c == '\\' && inDoubleQuote) {
+                if (i + 1 < input.length()) {
+                    char next = input.charAt(i + 1);
+
+                    if (next == '"' || next == '\\') {
+                        current.append(next);
+                        i++;
+                        continue;
+                    }
+                }
+                current.append('\\');
+                continue;
+            }
+
+            // WHITESPACE outside quotes
+            if (Character.isWhitespace(c) && !inSingleQuote && !inDoubleQuote) {
                 if (current.length() > 0) {
                     tokens.add(current.toString());
                     current.setLength(0);
                 }
+                continue;
             }
-            // Normal character
-            else {
-                current.append(c);
-            }
+
+            current.append(c);
         }
 
         if (current.length() > 0) {
