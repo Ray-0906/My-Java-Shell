1a3992579bac1de86e458b6bc86a663e5d63a736
Ray-0906 <astra3716@gmail.com>
2026-02-25 13:23:19 +0530

    codecrafters submit [skip ci]

--- src/main/java/Parser/Tokenizer.java
+++ src/main/java/Parser/Tokenizer.java
@@ -4,55 +4,89 @@ import java.util.List;
 
 public class Tokenizer {
     public static List<String> tokenize(String input) {
-    List<String> tokens = new java.util.ArrayList<>();
-    StringBuilder current = new StringBuilder();
+        List<String> tokens = new java.util.ArrayList<>();
+        StringBuilder current = new StringBuilder();
 
-    boolean inSingleQuote = false;
-    boolean inDoubleQuote = false;
-    boolean isbackslash = false;
-    for (int i = 0; i < input.length(); i++) {
-        char c = input.charAt(i);
+        boolean inSingleQuote = false;
+        boolean inDoubleQuote = false;
 
-        // HANDLE BACKSLASH FIRST (inside double quotes only)
-        if (isbackslash ) {
-            current.append(c);
-            isbackslash = false;
-            continue;
-           
-        }   
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
 
-        if (c == '\\' && !inSingleQuote) {
-            isbackslash = true;
-            continue;
-        }
-        // SINGLE QUOTES
-        if (c == '\'' && !inDoubleQuote) {
-            inSingleQuote = !inSingleQuote;
-            continue;
-        }
-
-        // DOUBLE QUOTES
-        if (c == '"' && !inSingleQuote) {
-            inDoubleQuote = !inDoubleQuote;
-            continue;
-        }
-
-        // WHITESPACE outside quotes
-        if (Character.isWhitespace(c) && !inSingleQuote && !inDoubleQuote) {
-            if (current.length() > 0) {
-                tokens.add(current.toString());
-                current.setLength(0);
+            // ===== SINGLE QUOTES =====
+            if (inSingleQuote) {
+                if (c == '\'') {
+                    inSingleQuote = false;
+                } else {
+                    current.append(c);
+                }
+                continue;
             }
-            continue;
+
+            // ===== DOUBLE QUOTES =====
+            if (inDoubleQuote) {
+
+                if (c == '\\') {
+                    if (i + 1 < input.length()) {
+                        char next = input.charAt(i + 1);
+
+                        if (next == '"' || next == '\\' || next == '$' || next == '`') {
+                            current.append(next);
+                            i++;
+                        } else {
+                            // literal backslash
+                            current.append('\\');
+                        }
+                    } else {
+                        current.append('\\');
+                    }
+                    continue;
+                }
+
+                if (c == '"') {
+                    inDoubleQuote = false;
+                    continue;
+                }
+
+                current.append(c);
+                continue;
+            }
+
+            // ===== NORMAL (outside quotes) =====
+
+            if (c == '\\') {
+                if (i + 1 < input.length()) {
+                    current.append(input.charAt(i + 1));
+                    i++;
+                }
+                continue;
+            }
+
+            if (c == '\'') {
+                inSingleQuote = true;
+                continue;
+            }
+
+            if (c == '"') {
+                inDoubleQuote = true;
+                continue;
+            }
+
+            if (Character.isWhitespace(c)) {
+                if (current.length() > 0) {
+                    tokens.add(current.toString());
+                    current.setLength(0);
+                }
+                continue;
+            }
+
+            current.append(c);
         }
 
-        current.append(c);
-    }
+        if (current.length() > 0) {
+            tokens.add(current.toString());
+        }
 
-    if (current.length() > 0) {
-        tokens.add(current.toString());
+        return tokens;
     }
-
-    return tokens;
-}
 }
