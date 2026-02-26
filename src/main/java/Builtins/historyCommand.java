2a22d729c7c445b7ad9c895a02ffcd76c0ed255e
Ray-0906 <astra3716@gmail.com>
2026-02-26 16:23:13 +0530

    codecrafters submit [skip ci]

--- src/main/java/Builtins/historyCommand.java
+++ src/main/java/Builtins/historyCommand.java
@@ -3,10 +3,30 @@ package Builtins;
 import Model.Command;
 import IO.ShellIo;
 
+import java.util.List;
+
 public class historyCommand {
     public static void execute(Command command, ShellIo io) {
-        for (int i = 1; i <= ShellContext.ShellContext.getHistory().size(); i++) {
-            io.println(String.format("%5d  %s", i, ShellContext.ShellContext.getHistory().get(i - 1)));
+        List<String> history = ShellContext.ShellContext.getHistory();
+        int total = history.size();
+
+        // Default: show all history
+        int count = total;
+
+        // If argument provided, limit to last n entries
+        if (command.getArgs().size() > 1) {
+            try {
+                count = Integer.parseInt(command.getArgs().get(1));
+            } catch (NumberFormatException e) {
+                // ignore, show all
+            }
+        }
+
+        // Calculate start index
+        int start = Math.max(0, total - count);
+
+        for (int i = start; i < total; i++) {
+            io.println(String.format("%5d  %s", i + 1, history.get(i)));
         }
     }
 }
